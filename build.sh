ba2cf32d3808d33a9df75f99ee28377cfec07152
Pingshun Ge <pge@arubanetworks.com>
2018-08-09 15:54:10 +0800

    Add developer

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Removing www..."
+rm -rf ./www/
+
+echo "Copying latest resources..."
+cp -r ./css ./www/
+cp -r ./fonts ./www/
+cp -r ./img ./www/
+cp -r ./js ./www/
+cp -r ./mail ./www/
+cp ./index.html ./www/
+
+echo "Done!"
