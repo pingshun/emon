d7bede66c37a0b4f192ab02a8781411f3bc62638
Pingshun Ge <pge@arubanetworks.com>
2018-08-10 19:00:04 +0800

    Change path of build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ echo "Removing www..."
 rm -rf ./www/*
 
 echo "Copying latest resources..."
-cp -r ./css ./www/
-cp -r ./fonts ./www/
-cp -r ./img ./www/
-cp -r ./js ./www/
-cp -r ./mail ./www/
+cp -r ./assets ./www/
 cp ./index.html ./www/
 cp ./baidu_verify_g1Ld5WiiIO.html ./www/
 
