0d1f5e47526a1830f8da85fd95d568ccd3b5670b
Pingshun Ge <pge@arubanetworks.com>
2018-08-17 17:04:57 +0800

    Add build image script to run.sh

--- image/run.sh
+++ image/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+image=`docker images | grep emontech/emon`
+if [ -z "$image" ]; then
+  echo 'No emon image found, build it first:'
+  docker build -t emontech/emon -f Dockerfile .
+else
+  echo 'emon image has been built:'
+  echo $image
+fi
+
 docker run -d -p 8081:80 --name emon -v $PWD/../www:/var/www/emon emontech/emon
